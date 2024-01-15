0bfa75d2e598c8addfffff09c76b7ece3b65bfc4
Brian <brianmariga0@gmail.com>
2024-01-15 07:00:34 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addTodo(e.target.new_task_description.value);
+    form.reset;
+  });
 });
+
+function addTodo(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = " x ";
+  li.textContent = todo;
+  li.appendChild(btn);
+  document.querySelector("ul").appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
